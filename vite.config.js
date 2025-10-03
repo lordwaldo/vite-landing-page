712fd5a2d48eab8794712128cd0ab9f09c5485be
lordwaldo <116594994+lordwaldo@users.noreply.github.com>
2025-10-03 13:42:06 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base: "/vite-landing-page"
+  base:'./', "/vite-landing-page"
 })
